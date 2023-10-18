5b045e1e322b77ff4983fe41cc362f3c80a0863c
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-10-18 17:55:43 +0600

    Переделка проекта для работы с БД. + Решение задачи.

--- main.py
+++ main.py
@@ -1,78 +1,145 @@
 import uvicorn
-from pydantic import BaseModel
-from typing import Annotated
-from fastapi import FastAPI, Request, HTTPException, Form
-from fastapi.responses import HTMLResponse
-import fastapi.templating
+import datetime
+from fastapi import FastAPI, HTTPException
+import database as db
+import models
+from typing import List
+from random import randint
 
 app = FastAPI()
-templates = fastapi.templating.Jinja2Templates(directory="templates")
-
-users = []
 
 
-class UserIn(BaseModel):
-    name: str
-    email: str
-    password: str
+@app.get("/")
+def root():
+    return {"Сообщение": "Привет"}
 
 
-class UserOut(BaseModel):
-    id: int
-    name: str
-    email: str
+@app.get("/fake_users/{count}")
+async def create_note(count: int):
+    for i in range(count):
+        query = db.users.insert().values(name=f'user{i}', surname=f'surname{i}', email=f'mail{i}@mail.ru',
+                                         password=f'qwerty{i}')
+        await db.database.execute(query)
+    return {'Сообщение': f'Cоздано {count} рандомных пользователей.'}
 
 
-class User(UserIn):
-    id: int
+@app.get("/fake_products/{count}")
+async def create_note(count: int):
+    for i in range(count):
+        query = db.products.insert().values(title=f'product {i}', description=f'all about product {i}',
+                                            price=randint(1, 100000))
+        await db.database.execute(query)
+    return {'Сообщение': f'Cоздано {count} рандомных товара.'}
 
 
-@app.get("/users/", response_class=HTMLResponse)
-async def get_users(request: Request):
-    return templates.TemplateResponse('users.html', {'request': request, 'users': users})
+@app.get("/fake_orders/{count}")
+async def create_note(count: int):
+    for i in range(count):
+        query = db.orders.insert().values(user_id=randint(1, 20), prod_id=randint(1, 20), status="created",
+                                          date=datetime.datetime.now())
+        await db.database.execute(query)
+    return {'Сообщение': f'Cоздано {count} рандомных заказа.'}
 
 
-@app.post("/users/", response_model=UserOut)
-async def add_user(user_in: UserIn):
-    new_user_id = users[-1].id + 1 if len(users) else 1
-    user = User(id=new_user_id, name=user_in.name, email=user_in.email, password=user_in.password)
-    users.append(user)
-    return UserOut(id=user.id, name=user.name, email=user.email)
+# read all users/products/orders
+
+@app.get("/users/", response_model=List[models.UserRead])
+async def read_users():
+    query = db.users.select()
+    return await db.database.fetch_all(query)
 
 
-@app.put("/users/", response_model=UserOut)
-async def edit_user(user_id: int, user_in: UserIn):
-    for user in users:
-        if user.id == user_id:
-            user.name = user_in.name
-            user.email = user_in.email
-            user.password = user_in.password
-            return UserOut(id=user.id, name=user.name, email=user.email)
-    raise HTTPException(status_code=404, detail='Пользователь не найден!')
+@app.get("/products/", response_model=List[models.ProductRead])
+async def read_products():
+    query = db.products.select()
+    return await db.database.fetch_all(query)
 
 
-@app.delete("/users/", response_model=dict)
+@app.get("/orders/", response_model=List[models.OrderRead])
+async def read_orders():
+    query = db.orders.select()
+    return await db.database.fetch_all(query)
+
+
+# read one user/product/order
+
+@app.get("/users/{user_id}", response_model=models.UserRead)
+async def read_user(user_id: int):
+    query = db.users.select().where(db.users.c.id == user_id)
+    user = await db.database.fetch_one(query)
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователей не найдены!")
+    return user
+
+
+@app.get("/products/{product_id}", response_model=models.ProductRead)
+async def read_product(product_id: int):
+    query = db.products.select().where(db.products.c.id == product_id)
+    product = await db.database.fetch_one(query)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Товары не найдены!")
+    return product
+
+
+@app.get("/orders/{order_id}", response_model=models.OrderRead)
+async def read_order(order_id: int):
+    query = db.orders.select().where(db.orders.c.id == order_id)
+    order = await db.database.fetch_one(query)
+    if order is None:
+        raise HTTPException(status_code=404, detail="Заказы не найдены!")
+    return order
+
+
+# update user/product/order
+
+@app.put("/users/{user_id}", response_model=models.UserRead)
+async def update_user(user_id: int, new_user: models.UserCreate):
+    query = db.users.update().where(db.users.c.id == user_id).values(**new_user.dict())
+    await db.database.execute(query)
+    return {**new_user.dict(), "id": user_id}
+
+
+@app.put("/products/{product_id}", response_model=models.ProductRead)
+async def update_product(product_id: int, new_product: models.ProductCreate):
+    query = db.products.update().where(db.products.c.id == product_id).values(**new_product.dict())
+    await db.database.execute(query)
+    return {**new_product.dict(), "id": product_id}
+
+
+@app.put("/orders/{order_id}", response_model=models.OrderRead)
+async def update_order(order_id: int, new_order: models.OrderCreate):
+    query = db.orders.update().where(db.orders.c.id == order_id).values(**new_order.dict())
+    await db.database.execute(query)
+    return {**new_order.dict(), "id": order_id}
+
+
+# delete user/product/order
+
+@app.delete("/users/{user_id}")
 async def delete_user(user_id: int):
-    for user in users:
-        if user.id == user_id:
-            users.remove(user)
-            return {'message': 'Пользователь был успешно удален.'}
-    raise HTTPException(status_code=404, detail='Пользователь не найден!')
+    query = db.users.delete().where(db.users.c.id == user_id)
+    await db.database.execute(query)
+    return {'Сообщение': 'Пользователей удален!'}
 
 
-@app.get("/new_user/", response_class=HTMLResponse)
-async def new_user(request: Request):
-    return templates.TemplateResponse('new_user.html', {'request': request})
+@app.delete("/products/{product_id}")
+async def delete_product(product_id: int):
+    query = db.products.delete().where(db.products.c.id == product_id)
+    await db.database.execute(query)
+    return {'Сообщение': 'Товар удален!'}
 
 
-@app.post("/new_user/", response_class=HTMLResponse)
-async def create_user(request: Request,
-                      user_name: Annotated[str, Form()],
-                      user_email: Annotated[str, Form()],
-                      user_password: Annotated[str, Form()]):
-    await add_user(UserIn(name=user_name, email=user_email, password=user_password))
-    return await get_users(request)
+@app.delete("/orders/{order_id}")
+async def delete_order(order_id: int):
+    query = db.orders.delete().where(db.orders.c.id == order_id)
+    await db.database.execute(query)
+    return {'Сообщение': 'Заказ удален!'}
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
